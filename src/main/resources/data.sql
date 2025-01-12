7c0c8504b3f4c86d3cd3293954d8864d907ebe23
Gennady <wkg@bk.ru>
2025-01-12 19:22:20 +0300

    add style, fix data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,25 @@
 insert into user (first_name, last_name, age, email, password)
-values ('Admin', 'Adminov', 20, 'admin', '$2a$10$XqhidiGP2qmGJ/j.4ZWIu.73sAxzqx3DaQzfog/3rCeUZee3fcXai');
+values ('admin', 'admin', 20, 'admin', '$2a$10$XqhidiGP2qmGJ/j.4ZWIu.73sAxzqx3DaQzfog/3rCeUZee3fcXai');
 insert into user (first_name, last_name, age, email, password)
-values ('User', 'Userson', 10, 'user', '$2a$10$xTfGvf6E./HJxELXL1w13O2H/XNOW3p29EmrfK7yX42FAhogV1ZYe');
+values ('user', 'user', 10, 'user', '$2a$10$xTfGvf6E./HJxELXL1w13O2H/XNOW3p29EmrfK7yX42FAhogV1ZYe');
+
+
+insert into user (first_name, last_name, age, email, password)
+values ('Joe', 'Biden', 93, 'joe@gmail.com', '$2a$10$xTfGvf6E./HJxELXL1w13O2H/XNOW3p29EmrfK7yX42FAhogV1ZYe');
+insert into user (first_name, last_name, age, email, password)
+values ('Donald', 'Trump', 94, 'donny@gmail.com', '$2a$10$xTfGvf6E./HJxELXL1w13O2H/XNOW3p29EmrfK7yX42FAhogV1ZYe');
+insert into user (first_name, last_name, age, email, password)
+values ('Barack', 'Obama', 95, 'barack@gmail.com', '$2a$10$xTfGvf6E./HJxELXL1w13O2H/XNOW3p29EmrfK7yX42FAhogV1ZYe');
+insert into user (first_name, last_name, age, email, password)
+values ('George', 'Bush', 96, 'junior@gmail.com', '$2a$10$xTfGvf6E./HJxELXL1w13O2H/XNOW3p29EmrfK7yX42FAhogV1ZYe');
+insert into user (first_name, last_name, age, email, password)
+values ('Bill', 'Clinton', 97, 'billy@gmail.com', '$2a$10$xTfGvf6E./HJxELXL1w13O2H/XNOW3p29EmrfK7yX42FAhogV1ZYe');
+insert into user (first_name, last_name, age, email, password)
+values ('Ronald', 'Reagan', 98, 'ronny@gmail.com', '$2a$10$xTfGvf6E./HJxELXL1w13O2H/XNOW3p29EmrfK7yX42FAhogV1ZYe');
+insert into user (first_name, last_name, age, email, password)
+values ('Jimmy', 'Carter', 99, 'jimmy@gmail.com', '$2a$10$xTfGvf6E./HJxELXL1w13O2H/XNOW3p29EmrfK7yX42FAhogV1ZYe');
+insert into user (first_name, last_name, age, email, password)
+values ('Richard', 'Nixon', 100, 'richard@gmail.com', '$2a$10$xTfGvf6E./HJxELXL1w13O2H/XNOW3p29EmrfK7yX42FAhogV1ZYe');
 
 insert into role (role)
 values ('ADMIN');
@@ -14,3 +32,15 @@ insert into user_role (user_id, role_id)
 values (1, 1);
 insert into user_role (user_id, role_id)
 values (2, 2);
+
+insert into user_role (user_id, role_id) values (3, 2);
+insert into user_role (user_id, role_id) values (4, 2);
+insert into user_role (user_id, role_id) values (5, 2);
+insert into user_role (user_id, role_id) values (6, 2);
+insert into user_role (user_id, role_id) values (7, 2);
+insert into user_role (user_id, role_id) values (8, 2);
+insert into user_role (user_id, role_id) values (9, 2);
+insert into user_role (user_id, role_id) values (10, 2);
+
+# login: user password: user
+# login: admin password: admin
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,27 +5,24 @@ drop table if exists user;
 create table if not exists role
 (
     role_id bigint auto_increment primary key,
-    role    varchar(255) null
+    role    varchar(255) not null unique
 );
 
 create table if not exists user
 (
-    user_id bigint auto_increment primary key,
-    age        tinyint      null,
-    email      varchar(255) null,
-    first_name varchar(50)  null,
-    last_name  varchar(50)  null,
-    password   varchar(255) null,
-    constraint UK_ob8kqyqqgmefl0aco34akdtpe
-        unique (email)
+    user_id    bigint auto_increment primary key,
+    age        tinyint      not null,
+    email      varchar(255) not null unique,
+    first_name varchar(50)  not null,
+    last_name  varchar(50)  not null,
+    password   varchar(255) not null
 );
 
 create table if not exists user_role
 (
     user_id bigint not null,
     role_id bigint not null,
-    constraint FK859n2jvi8ivhui0rl0esws6o
-        foreign key (user_id) references user (user_id),
-    constraint FKa68196081fvovjhkek5m97n3y
-        foreign key (role_id) references role (role_id)
+    primary key (user_id, role_id),
+    foreign key (user_id) references user (user_id),
+    foreign key (role_id) references role (role_id)
 );
