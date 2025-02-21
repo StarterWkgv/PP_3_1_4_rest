c88a23c888a719fae472109c4a59fa5da04daf2a
Gennady <wkg@bk.ru>
2025-02-21 18:24:53 +0300

    add header

--- src/main/resources/static/js/userscript.js
+++ src/main/resources/static/js/userscript.js
@@ -1,7 +1,7 @@
 const getUpdateTable = (() => {
-    const USERS_URL = "/api/user";
-    const USERS_TABLE = "userInfo";
-
+    const USER_URL = "/api/user";
+    const USER_TABLE = "userInfo";
+    const HEADER_ID = "navbar-username";
     const row = user => {
         let out = '<tr>';
         Object.keys(user).forEach(k => {
@@ -12,6 +12,12 @@ const getUpdateTable = (() => {
         return out;
     };
 
+    const header = user => {
+        let out = `<strong >${user['email']}</strong>
+        with roles:
+        <span>${user['roles'].reduce((a, b) => a + b, "")}</span>`;
+    };
+
     const getUpdateTable = (url, row, table) => {
         return async () => {
             try {
@@ -19,7 +25,7 @@ const getUpdateTable = (() => {
                 const resp = await fetch(url);
                 if (resp.ok) {
                     let users = await resp.json();
-                    document.getElementById(table).innerHTML = Array.of(users).flatMap(a=>a).map(row).reduce((a, b) => a + b, "");
+                    document.getElementById(table).innerHTML = Array.of(users).flatMap(a => a).map(row).reduce((a, b) => a + b, "");
                 } else {
                     console.error(resp.status, " Couldn't get the data from server")
                 }
@@ -30,8 +36,9 @@ const getUpdateTable = (() => {
     };
 
     const init = async () => {
-        const updateTable =  getUpdateTable(USERS_URL, row, USERS_TABLE);
-        $('#v-pills-user-tab').on('show.bs.tab',updateTable );
+        const updateTable = getUpdateTable(USER_URL, row, USER_TABLE);
+        $('#v-pills-user-tab').on('show.bs.tab', updateTable);
+        await getUpdateTable(USER_URL, header, HEADER_ID)();
         await updateTable();
     }
 
